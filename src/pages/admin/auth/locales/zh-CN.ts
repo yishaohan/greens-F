5a2b00cc89044f975dfa4a5d63144df6154b46ce
YCZ <YICUNZHI@icloud.com>
2021-02-08 09:55:33 -0800

    新增权限管理

---
+++ src/pages/admin/auth/locales/zh-CN.ts
@@ -0,0 +1,13 @@
+export default {
+  'createAuth.authName.required': '请输入权限名称!',
+  'createAuth.authName.placeholder': '请输入权限名称!',
+  'createAuth.authDescript.required': '请输入权限描述!',
+  'createAuth.authDescript.placeholder': '请输入权限描述!',
+  'createAuth.requestUrl.required': '请输入请求地址!',
+  'createAuth.requestUrl.placeholder': '请输入请求地址!',
+  'createAuth.requestMethod.required': '请输入请求方法!',
+  'editAuth.authName.required': '请输入权限名称!',
+  'editAuth.authDescript.required': '请输入权限描述!',
+  'editAuth.requestUrl.required': '请输入请求地址!',
+  'editAuth.requestMethod.required': '请输入请求方法!',
+};
